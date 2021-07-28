296a480d433c354c310b5583929e5c2f5941f392
Dexton Anderson <contact@dextonanderson.com>
2021-07-28 14:25:00 -0500

    Change exe name, denote unsigned

--- compile_win.sh
+++ compile_win.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pyinstaller -w --icon=icon.ico --onefile --name="SteveCinemaInstaller" installer.py
+pyinstaller -w --icon=icon.ico --onefile --name="SteveCinemaInstaller_unsigned" installer.py
